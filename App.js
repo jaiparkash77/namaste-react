cbe9f8f5f69b3b857c8c4c15164b0c01505f3b7e
Jai Parkash <developerjaiparkash@gmail.com>
2024-06-30 17:33:59 +0530

    chore: creating nested structure in react

--- App.js
+++ App.js
@@ -1,7 +1,56 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello world from react");
+/*
+*
+*<div id="parent">
+*    <div id="child">
+*        <h1>Heading 1</h1>
+*       <h2>Heading 2</h2>
+*    </div>
+*    <div id="child2">
+*        <h1>Heading 1</h1>
+*       <h2>Heading 2</h2>
+*    </div>
+*</div>
+*
+*/
+const parent = React.createElement(
+    "div", 
+    { id: "parent" }, 
+    [
+        React.createElement(
+            "div", 
+            { id: "child" }, 
+            [
+                React.createElement(
+                    "h1", 
+                    { id: "heading" }, 
+                    "Heading 1"
+                ), React.createElement(
+                    "h2", 
+                    { id: "heading" }, 
+                    "Heading 2"
+                )
+            ]
+        ),
+        React.createElement(
+            "div", 
+            { id: "child2" }, 
+            [
+                React.createElement(
+                    "h1", 
+                    { id: "heading" }, 
+                    "Heading 1"
+                ), React.createElement(
+                    "h2", 
+                    { id: "heading" }, 
+                    "Heading 2"
+                )
+            ]
+        )
+    ]
+);
 
-console.log(heading); //object
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
