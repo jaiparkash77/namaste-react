ec738fecb120b0763189a03238d0c5fd9d2c2435
Jai Parkash <developerjaiparkash@gmail.com>
2024-07-14 19:05:49 +0530

    Creating Food App using functional component

--- App.js
+++ App.js
@@ -1,47 +1,79 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = (
-    <h1 id="heading">
-        Namaste React using JSX. This is react element
-    </h1>
-);
+/*
+* Header
+*   - Logo
+*   - Nav Items
+* Body
+*   - Search
+*   - RestaurantContainer
+*       - RestaurantCard
+*           - Img
+*           - Name of Resaurant, Star rating, Cuisine, delivery time
+* Footer
+*   - Copyright
+*   - Links
+*   - Address
+*   - Contact
+*/
 
-// React Component
-// Class Based Component - OLD
-// Functional Component - NEW
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+};
 
-// React Functional Component
-
-const Title = () => (
-    <h1 id="heading">
-        Namaste React using JSX
-    </h1>
-);
-
-const HeadingComponent = () =>{
-    return <h1>Namaste React Functional Component</h1>
+const styleCard = {
+    backgroundColor: "#f0f0f0"
 }
 
-const HeadingComponent2 = () => <h1>Namaste React Functional Component</h1>;
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi" alt="res-logo" />
+            <h3>Mehgana Foods</h3>
+            <h5>Biryani, North Indian, Asian</h5>
+            <h5>4.3 Stars</h5>
+        </div>
+    )
+}
 
-// Component Composition - composing multiple component into single
-const HeadingComponent3 = () => (
-    <div id="container">
-        {heading}
-        <Title />
-        {Title()}
-        <Title></Title>
-        <h1>Namaste React Functional Component</h1>
-    </div>
-)
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
 
-
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Rendering react element
-// root.render(heading);
-
-// Rendering react component
-root.render(<HeadingComponent3 />);
+root.render(<AppLayout />);
