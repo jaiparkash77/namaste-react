2cc20ca6cf899fa21ca546639782659314cfc45d
Jai Parkash <developerjaiparkash@gmail.com>
2024-08-11 13:17:17 +0530

    chore: React routing and error handeling

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <h2>This is about page of namaste react.</h2>
+    </div>
+  )
+}
+
+export default About
