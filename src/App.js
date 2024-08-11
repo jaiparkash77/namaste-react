2c33e4224c34457c011300d7826459699a984753
Jai Parkash <developerjaiparkash@gmail.com>
2024-08-11 14:38:09 +0530

    chore: Children routes in react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -11,7 +11,8 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            {/* Outlet will be filled according to path defined in children of AppLayout */}
+            <Outlet />
         </div>
     )
 };
@@ -20,16 +21,22 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Body />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/contact",
+                element: <Contact />
+            }
+        ],
         errorElement: <Error />
     },
-    {
-        path: "/about",
-        element: <About />
-    },
-    {
-        path: "/contact",
-        element: <Contact />
-    }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
