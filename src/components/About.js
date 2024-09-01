6541a84be1dc3dd2718ae98f7d11891fb6ff7f1e
Jai Parkash <developerjaiparkash@gmail.com>
2024-09-01 21:49:35 +0530

    functional and class based component with props

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import User from './User'
+import UserClass from './UserClass';
 
 const About = () => {
   return (
     <div>
         <h1>About Page</h1>
         <h2>This is about page of namaste react.</h2>
+        {/* Functional Component */}
+        <User name={"Jai Parkash (function)"} />
+
+        {/* Class Component */}
+        <UserClass name={"Jai Parkash (class)"} location={"Delhi (class)"} />
     </div>
   )
 }
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+
+const User = (props) => {
+    return (
+        <div className="user-card">
+            <h2>Name: {props.name}</h2>
+            <h3>Location: Delhi</h3>
+            <h4>Contact: @jaiparkash77</h4>
+        </div>
+    )
+}
+
+export default User;
