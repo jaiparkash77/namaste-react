2a2bca5f206649c2592b5f4be783c1a96a898f42
Jai Parkash <developerjaiparkash@gmail.com>
2024-10-05 17:22:42 +0530

    Single responsibility principal - custom hook to get online status

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
     // Local State Variable - Super powerful variable
@@ -22,6 +23,10 @@ const Body = () => {
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
+
+    const onlineStatus = useOnlineStatus();
+
+    if(!onlineStatus) return <h1>Looks like you are offline!! Please check your internet connection.</h1>
 
     return filteredRestaurants.length === 0 ? ( <Shimmer /> ) : (
         <div className="body">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
     let [btnName, setBtnName] = useState("Login");
+
+    const onlineStatus = useOnlineStatus();
 
     return (
         <div className="header">
@@ -12,6 +15,7 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>Online Status: {onlineStatus ? '🧶' :'🎈'}</li>
                     <li><Link to={"/"}>Home</Link></li>
                     <li><Link to={"/about"}>About Us</Link></li>
                     <li><Link to={"/contact"}>Contact Us</Link></li>
